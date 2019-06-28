2a3c0075a11db014e531e09a10bef14851933fa8
YJUNY <36507979+YOUNGJUN2@users.noreply.github.com>
2019-06-28 22:32:04 +0900

    Update apk_repack.py

--- apk_repack.py
+++ apk_repack.py
@@ -1,4 +1,4 @@
-import subprocess
+from subprocess import run
 from sys import argv
 
 file = argv[1]
@@ -6,6 +6,6 @@ file = argv[1]
 apkTool = "C:\\APKTOOL\\"
 signApk = "C:\\Users\\cortk\\Desktop\\SignApk\\"
 
-subprocess.run("java -jar %sapktool.jar b %s -o %s.apk" % (apkTool, file, file))
-subprocess.run("java -jar %ssignapk.jar %scertificate.pem %skey.pk8 %s.apk %s_sined.apk" % (signApk, signApk, signApk, file, file))
-subprocess.run("adb install %s_sined.apk" % (file))
+run("java -jar %sapktool.jar b %s -o %s.apk" % (apkTool, file, file))
+run("java -jar %ssignapk.jar %scertificate.pem %skey.pk8 %s.apk %s_sined.apk" % (signApk, signApk, signApk, file, file))
+run("adb install %s_sined.apk" % (file))
